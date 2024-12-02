f86f4da38ba7ee3b9291e2b6cc5f8cd3d505a312
Mykhailo Kravchenko <rezkiy37@gmail.com>
2024-12-02 13:09:26 +0100

    rewrite js

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-function processElements() {
-	const sections = document.querySelectorAll('.selection__section')
+document.addEventListener("DOMContentLoaded", () => {
+  const sections = document.querySelectorAll(".selection__section");
 
-	sections.forEach((section) => {
-		section.addEventListener('click', () => {
-			const content = section.nextElementSibling
+  sections.forEach((section) => {
+    section.addEventListener("click", () => {
+      const content = section.nextElementSibling;
 
-			if (content && content.classList.contains('selection__content')) {
-				content.classList.toggle('hidden')
-			}
-		})
-	})
-}
-
-processElements()
+      if (content && content.classList.contains("selection__content")) {
+        content.classList.toggle("hidden");
+      }
+    });
+  });
+});
