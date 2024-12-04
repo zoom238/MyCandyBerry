93c4aa27464ec32523cf6de8ff5ecf397e39a494
Ivan Kravchenko <37vanya.komp69@gmail.com>
2024-12-04 01:38:14 +0100

    Added footer and working on animation

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function processElements() {
+document.addEventListener('DOMContentLoaded', () => {
 	const sections = document.querySelectorAll('.selection__section')
 
 	sections.forEach((section) => {
@@ -10,6 +10,22 @@ function processElements() {
 			}
 		})
 	})
-}
+})
 
-processElements()
+function toggleBlock() {
+	const block = document.querySelector('.nav__content-hidden')
+	block.classList.toggle('visible')
+}
+// function processElements() {
+// 	const sections = document.querySelectorAll('.selection__section')
+
+// 	sections.forEach((section) => {
+// 		section.addEventListener('click', () => {
+// 			const content = section.nextElementSibling
+
+// 			if (content && content.classList.contains('selection__content')) {
+// 				content.classList.toggle('hidden')
+// 			}
+// 		})
+// 	})
+//er
