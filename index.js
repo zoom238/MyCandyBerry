4c32b19915762558b5044acf0374f5cd593b0ca6
Ivan Kravchenko <37vanya.komp69@gmail.com>
2024-12-05 00:50:47 +0100

    continue working on nav and transition there

--- index.js
+++ index.js
@@ -10,22 +10,16 @@ document.addEventListener('DOMContentLoaded', () => {
 			}
 		})
 	})
+
+	const nav = document.querySelectorAll('.nav')
+
+	nav.forEach((nav_item) => {
+		nav_item.addEventListener('click', () => {
+			const nav_content = nav_item.nextElementSibling
+
+			if (nav_content && nav_content.classList.contains('nav_content_item')) {
+				nav_content.classList.toggle('visible')
+			}
+		})
+	})
 })
-
-function toggleBlock() {
-	const block = document.querySelector('.nav__content-hidden')
-	block.classList.toggle('visible')
-}
-// function processElements() {
-// 	const sections = document.querySelectorAll('.selection__section')
-
-// 	sections.forEach((section) => {
-// 		section.addEventListener('click', () => {
-// 			const content = section.nextElementSibling
-
-// 			if (content && content.classList.contains('selection__content')) {
-// 				content.classList.toggle('hidden')
-// 			}
-// 		})
-// 	})
-//er
